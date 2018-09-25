c18df95a85515fba19b0ebb96ea28a3634cfd8c7
伍家发 <wujiafa@hedemac-mini.local>
2018-09-25 14:25:51 +0800

    Initial Commit

---
+++ HHYP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HHYP
+//
+//  Created by 伍家发 on 2018/9/25.
+//  Copyright © 2018年 伍家发. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HHYP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HHYP
+//
+//  Created by 伍家发 on 2018/9/25.
+//  Copyright © 2018年 伍家发. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
